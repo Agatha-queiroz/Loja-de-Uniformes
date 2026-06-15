3483737ce5d11a701b323b0adce12084d2405931
mLui-a <159588322+mLui-a@users.noreply.github.com>
2026-06-14 21:36:19 -0300

    Refactor ProdutoLista component for consistency

--- src/assets/componentes/ProdutoLista.jsx
+++ src/assets/componentes/ProdutoLista.jsx
@@ -1,18 +1,21 @@
 import ProdutoCard from "./ProdutoCard";
 
-function ProdutoLista({produtos, onAddAoCart}){
-    return(
-        <div className="produto-grid">
-        {produtos.map((produto) => (
-          <ProdutoCard
-            key={produto.id}
-            imagem={produto.imagem}
-            nome={produto.nome}
-            preco={produto.preco}
-            onAddAoCart={onAddAoCart}
-          />
-        ))}
-      </div>
-    );
+function ProdutoLista({produtos, onAddAoCart,}) {
+  return (
+    <div className="produto-grid">
+
+      {produtos.map((produto) => (
+        <ProdutoCard
+          key={produto.id}
+          imagem={produto.imagem}
+          nome={produto.nome}
+          preco={produto.preco}
+          onAddAoCart={onAddAoCart}
+        />
+      ))}
+
+    </div>
+  );
 }
+
 export default ProdutoLista;
