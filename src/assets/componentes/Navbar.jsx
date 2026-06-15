2acfdb989c175c26fe859c5f57085311247e1b04
mLui-a <159588322+mLui-a@users.noreply.github.com>
2026-06-14 21:37:49 -0300

    Refactor Navbar to include onCartClick handler
    Added onCartClick prop to Navbar for cart interaction.

--- src/assets/componentes/Navbar.jsx
+++ src/assets/componentes/Navbar.jsx
@@ -1,19 +1,20 @@
-import SideBar from "../componentes/SideBar.jsx"
 import Logo from '../imagem/LogoMarca.png'
 import compras from '../imagem/compras.png'
 import Procurar from '../imagem/procurar.png'
 
-function Navbar({cartContagem}) {
+function Navbar({cartContagem ,onCartClick}) {
   return (
     <nav className="navbar">
       <img  className="logo" src={Logo} alt="Logo da Empresa"/>
       
       <div className="pesquisar-box">
-      <input id="barra-pesq" type="text" placeholder="Pesquisar"/>{ /* isso vai funcionar :) onClick={()=> {props.openCart(true)}}*/ } 
+      <input id="barra-pesq" type="text" placeholder="Pesquisar"/>
       <button  className="btn-pesq"><img src={Procurar} alt="" /></button>
       </div>
 
-      <div className="navbar-cart"> Carrinho <img src={compras} alt="carrinho" /><span className="Cont-carr"> {cartContagem}</span></div>
+      <div className="navbar-cart" onClick={onCartClick}> Carrinho <img src={compras} alt="carrinho" /> 
+        <span className="Cont-carr"> {cartContagem} </span>
+      </div>
     </nav>
   );
 }
